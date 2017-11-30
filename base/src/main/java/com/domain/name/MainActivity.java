44083802f106b5ddcc24960b4109faac5c20bee3
Liux <lx0758@qq.com>
2017-11-30 10:10:57 +0800

    添加基础应用

---
+++ base/src/main/java/com/domain/name/MainActivity.java
@@ -0,0 +1,18 @@
+package com.domain.name;
+
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.v7.app.AppCompatActivity;
+
+/**
+ * Created by Liux on 2017/11/30.
+ */
+
+public class MainActivity extends AppCompatActivity {
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        setContentView(R.layout.activity_main);
+    }
+}
